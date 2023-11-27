ec8c35fd20b267b5f5e7118c925bda712ba17db9
Gabriel Tomsic <gabe@mac-m-two.local>
2023-11-27 09:27:51 -0600

    Initial commit

---
+++ src/components/navar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from './navbar.module.css';
+
+const Navbar = () => {
+   return;
+   <div className={styles.container}>
+      <div className={styles.social}></div>
+      <div className={styles.logo}></div>
+      <div className={styles.links}></div>
+   </div>;
+};
+export default Navbar;
